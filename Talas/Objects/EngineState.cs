84738d8ee4e7ce705aedc4a44ac3eeaf7a8912d6
Zver <Zver@ZVERDVD-3PPV2ID>
2017-04-07 22:46:21 +0300

    ref

--- Talas/Objects/EngineState.cs
+++ Talas/Objects/EngineState.cs
@@ -39,19 +39,19 @@ namespace Objects
             {
                 Double result=0;
                 if (R60.HasValue && R30.HasValue && Ravg.HasValue)
-                    switch (R30)
+                    switch (Ravg)//R30 => Ravg, R60 => R30, Ravg => R60
                     {
                         case 11:
-                            result = (Double)R60.Value / 10 + (Double)Ravg.Value / 1000;
+                            result = (Double)R30.Value / 10 + (Double)R60.Value / 1000;
                             break;
                         case 10:
-                            result = ((Double)R60.Value / 10 + (Double)Ravg.Value / 1000) / 1000;
+                            result = ((Double)R30.Value / 10 + (Double)R60.Value / 1000) / 1000;
                             break;
                         case 2:
-                            result = ((Double)R60.Value + ((Double)Ravg.Value / 100)) / 1000;
+                            result = ((Double)R30.Value + ((Double)R60.Value / 100)) / 1000;
                             break;
                         case 4:
-                            result = ((Double)R60.Value * 10 + (Double)Ravg.Value / 10) / 1000;
+                            result = ((Double)R30.Value * 10 + (Double)R60.Value / 10) / 1000;
                             break;
                     }
 
